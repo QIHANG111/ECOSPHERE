c2379732a41d0929e196401587a97f8ab6280643
herry <guoqh670@gmail.com>
2024-11-12 22:45:01 +0800

    reportPage

--- src/script/homepage.js
+++ src/script/homepage.js
@@ -11,13 +11,10 @@ menuItems.forEach((item) => {
     });
 });
 
-reportItems.forEach((item) => {
-    item.addEventListener("click", () => {
-        const targetPage = item.getAttribute("data-target");
 
-        //redirect to the target page
-        if (targetPage) {
-            window.location.href = targetPage;
-        }
-    });
-});
+fetch('../assets/example/homepageReport.txt')
+    .then(response => response.text())
+    .then(data => {
+        document.getElementById('text-content').innerText = data;
+    })
+    .catch(error => console.error('Error fetching the text file:', error));
