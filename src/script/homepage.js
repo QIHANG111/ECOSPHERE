60886af939ea881f3c5b7e92ddb7c0abff71f12c
herry <guoqh670@gmail.com>
2024-11-12 19:51:59 +0800

    homePage

---
+++ src/script/homepage.js
@@ -0,0 +1,23 @@
+const menuItems = document.querySelectorAll(".menu-item");
+const reportItems = document.querySelectorAll(".reportCard");
+menuItems.forEach((item) => {
+    item.addEventListener("click", () => {
+        const targetPage = item.getAttribute("data-target");
+
+        //redirect to the target page
+        if (targetPage) {
+            window.location.href = targetPage;
+        }
+    });
+});
+
+reportItems.forEach((item) => {
+    item.addEventListener("click", () => {
+        const targetPage = item.getAttribute("data-target");
+
+        //redirect to the target page
+        if (targetPage) {
+            window.location.href = targetPage;
+        }
+    });
+});
