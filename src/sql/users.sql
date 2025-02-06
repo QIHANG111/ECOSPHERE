ede22950a40b0bc578ed192c85941fd7cabb6bfd
kyii wong <kyiiwong117@gmail.com>
2025-02-06 11:59:35 +0800

    test

--- src/sql/users.sql
+++ src/sql/users.sql
@@ -7,4 +7,19 @@ CREATE TABLE users (
                        email_or_phone VARCHAR(255) NOT NULL,
                        password VARCHAR(255) NOT NULL,
                        created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
-);
+)
+
+-- CREATE TABLE users (
+--                        user_id INT AUTO_INCREMENT PRIMARY KEY,
+--                        name VARCHAR() NOT NULL,
+--                        email VARCHAR(255) NOT NULL,
+--                        phone INT NOT NULL,
+--                        hashed_password VARCHAR(255) NOT NULL,
+--                        role_id FOREIGN KEY NOT NULL,
+-- )
+
+CREATE TABLE role (
+
+)
+
+;
