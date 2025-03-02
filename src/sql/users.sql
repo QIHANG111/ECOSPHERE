21b7d9cb246db0a25f0489da520df502d02cc070
Cheeyau04 <hcyau1206@gmail.com>
2025-03-02 15:10:43 +0800

    bug fix

--- src/sql/users.sql
+++ src/sql/users.sql
@@ -4,13 +4,14 @@ USE ecosystem;
 
 
 
-CREATE TABLE users (
+CREATE TABLE Users (
                     user_id INT AUTO_INCREMENT PRIMARY KEY,
-                    name VARCHAR() NOT NULL,
+                    name VARCHAR(255) NOT NULL,
                     email VARCHAR(255) NOT NULL,
-                    phone INT NOT NULL,
+                    phone VARCHAR(15) NOT NULL,
                     hashed_password VARCHAR(255) NOT NULL,
-                    role_id FOREIGN KEY NOT NULL
+                    role_id INT NOT NULL,
+                    FOREIGN KEY (role_id) REFERENCES Role(role_id)
 );
 
 
@@ -18,14 +19,14 @@ CREATE TABLE Devices (
                     device_id INT AUTO_INCREMENT PRIMARY KEY,
                     device_name VARCHAR(255) NOT NULL,
                     device_type VARCHAR(255) NOT NULL,
-                    Status VARCHAR(63) NOT NULL
+                    status VARCHAR(63) NOT NULL
 );
 
 CREATE TABLE Energy(
                     usage_id INT AUTO_INCREMENT NOT NULL,
                     device_id INT NOT NULL,
                     PRIMARY KEY (usage_id, device_id),
-                    FOREIGN KEY (device_id) REFERENCES devices(device_id),
+                    FOREIGN KEY (device_id) REFERENCES Devices(device_id),
                     recorded_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
                     energy_consumed DOUBLE(255, 2)
 );
@@ -50,22 +51,22 @@ CREATE TABLE RolePermission(
                     FOREIGN KEY (permission_id) REFERENCES Permission(permission_id)
 );
 
-INSERT INTO Permission 
-VALUES ('user_management', 'create_user', 'Create new user accounts'),
+INSERT INTO Permission (category, name, description) VALUES 
+('user_management', 'create_user', 'Create new user accounts'),
 ('user_management', 'delete_user', 'Delete user accounts'),
 ('user_management', 'update_user', 'Update user accounts'),
 ('user_management', 'view_user', 'View user accounts'),
 ('user_management', 'assign_user', 'Assign role to user accounts'),
-('device_control', 'add_device', 'Add a device to the system');
-('device_control', 'delete_device', 'Remove a device from the system');
-('device_control', 'modify_device', 'Modify connected device setting');
-('device_control', 'view_device', 'View connected device');
-('device_control', 'control_device', 'Turn devices ON/OFF remotely');
-('device_control', 'schedule_device', 'Set automoation schedule');
-('energy_monitoring', 'view_energy', 'View energy consumption reports');
-('energy_monitoring', 'compare_energy', 'Compare energy usage trends');
-('energy_monitoring', 'rec_energy', 'Receive energy saving recommendations');
-('energy_monitoring', 'alert_energy', 'Get alerts for high energy usage');
-('system', 'system_backup', 'Perform system backup');
-('system', 'system_restore', 'Restore system from backup');
+('device_control', 'add_device', 'Add a device to the system'),
+('device_control', 'delete_device', 'Remove a device from the system'),
+('device_control', 'modify_device', 'Modify connected device setting'),
+('device_control', 'view_device', 'View connected device'),
+('device_control', 'control_device', 'Turn devices ON/OFF remotely'),
+('device_control', 'schedule_device', 'Set automation schedule'),
+('energy_monitoring', 'view_energy', 'View energy consumption reports'),
+('energy_monitoring', 'compare_energy', 'Compare energy usage trends'),
+('energy_monitoring', 'rec_energy', 'Receive energy-saving recommendations'),
+('energy_monitoring', 'alert_energy', 'Get alerts for high energy usage'),
+('system', 'system_backup', 'Perform system backup'),
+('system', 'system_restore', 'Restore system from backup'),
 ('system', 'system_updates', 'Perform system updates');
