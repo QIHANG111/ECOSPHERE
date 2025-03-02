94a8c7cc63350e6fcb6af2f35cda287662668239
sanje28 <sanjeivarajah10@gmail.com>
2025-03-02 22:10:34 +0800

    connected mongodb to the server

--- src/server.js
+++ src/server.js
@@ -1,7 +1,10 @@
 import app from './app.js';
+import { connectDB } from './config/db.js';
 
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
+    connectDB();
     console.log(`Server is running at http://localhost:${PORT}`);
 });
+
