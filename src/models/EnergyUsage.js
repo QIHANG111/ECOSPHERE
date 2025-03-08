d189b5ab6dc15f7a4878f7cdbcc07aff2025cf80
kyii wong <kyiiwong117@gmail.com>
2025-03-08 21:13:13 +0800

    added energy consumed into database

---
+++ src/models/EnergyUsage.js
@@ -0,0 +1,9 @@
+import mongoose from "mongoose";
+
+const EnergyUsageSchema = new mongoose.Schema({
+    date: {type: Date, required: true },
+    energyusage: {type: Number, required: true }
+});
+
+const EnergyUsage = mongoose.model('EnergyUsage', EnergyUsageSchema);
+export default EnergyUsage;
