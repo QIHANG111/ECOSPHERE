2f78b9c300f892c2b6d23be4fe521053d705e104
kyii wong <kyiiwong117@gmail.com>
2025-03-11 23:52:40 +0800

    changed user api to fetch from mongodb

--- src/database/db.js
+++ src/database/db.js
@@ -2,7 +2,7 @@ import mongoose from 'mongoose';
 import dotenv from "dotenv";
 import fs from 'fs';
 import EnergyUsage from '../models/energy.model.js';
-import { faker } from '@faker-js/faker';
+import { faker } from '@faker-js/faker'; 
 
 dotenv.config();
 
